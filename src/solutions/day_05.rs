340b3d51cac6ea03a9d5f5663bc6d7b39186b250
Alex van de Sandt <alex@avandesa.dev>
2025-12-05 15:12:22 -0600

    Re-solve day 5 part 1 with `rangemap`

--- src/solutions/day_05.rs
+++ src/solutions/day_05.rs
@@ -1,20 +1,10 @@
-use std::collections::BTreeMap;
+use rangemap::RangeInclusiveSet;
 
 use crate::solution::Solution;
 
-#[derive(Clone, Copy, Debug)]
-struct Range(u64, u64);
-
-impl Range {
-    fn parse(line: &str) -> Self {
-        let (from, to) = line.split_once('-').unwrap();
-        Self(from.parse().unwrap(), to.parse().unwrap())
-    }
-}
-
 #[derive(Debug)]
 pub struct Day05 {
-    ranges: Vec<Range>,
+    ranges: RangeInclusiveSet<u64>,
     ingredients: Vec<u64>,
 }
 
@@ -22,7 +12,13 @@ impl Solution for Day05 {
     fn with_input(input: String) -> Self {
         let (ranges, ingredients) = input.split_once("\n\n").unwrap();
 
-        let ranges = ranges.lines().map(Range::parse).collect();
+        let ranges = ranges
+            .lines()
+            .map(|line| {
+                let (from, to) = line.split_once('-').unwrap();
+                from.parse().unwrap()..=to.parse().unwrap()
+            })
+            .collect();
         let ingredients = ingredients.lines().map(|l| l.parse().unwrap()).collect();
 
         Self {
@@ -32,19 +28,10 @@ impl Solution for Day05 {
     }
 
     fn part1(&self) -> String {
-        let mut ingredients_map = self
-            .ingredients
+        self.ingredients
             .iter()
-            .copied()
-            .map(|i| (i, false))
-            .collect::<BTreeMap<_, _>>();
-
-        for Range(from, to) in &self.ranges {
-            for (_, is_fresh) in ingredients_map.range_mut(from..=to) {
-                *is_fresh = true;
-            }
-        }
-
-        ingredients_map.values().filter(|v| **v).count().to_string()
+            .filter(|i| self.ranges.contains(i))
+            .count()
+            .to_string()
     }
 }
