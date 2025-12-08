139fadae6a5d87f587701b69a7703645207c5eef
Alex van de Sandt <alex@avandesa.dev>
2025-12-08 10:47:04 -0600

    Operate over bytes instead of chars

--- src/solutions/day_06.rs
+++ src/solutions/day_06.rs
@@ -19,34 +19,49 @@ impl Problem {
 
 #[derive(Debug)]
 pub struct Day06 {
-    input: String,
+    lines: Vec<Vec<u8>>,
+}
+
+fn bytes_to_u32(digits: &[u8]) -> u32 {
+    let pow = (digits.len() - 1) as u32;
+    digits
+        .iter()
+        .copied()
+        .enumerate()
+        .map(|(i, b)| {
+            debug_assert!(b.is_ascii_digit());
+            let d = (b - b'0') as u32;
+            10u32.pow(pow - i as u32) * d
+        })
+        .sum()
 }
 
 impl Day06 {
     fn parse_input_part1(&self) -> Vec<Problem> {
         let mut problem_inputs = Vec::<Vec<u32>>::new();
-        for line in self.input.lines() {
-            if matches!(line.chars().next().unwrap(), '+' | '*') {
-                let problems = problem_inputs
+        for line in &self.lines {
+            if matches!(line[0], b'+' | b'*') {
+                return problem_inputs
                     .into_iter()
-                    .zip(line.split_whitespace())
-                    .map(|(inputs, op)| match op {
-                        "+" => Problem::Add(inputs),
-                        "*" => Problem::Mul(inputs),
+                    .zip(line.split(|b| *b == b' ').filter(|slice| !slice.is_empty()))
+                    .map(|(inputs, op)| match op[0] {
+                        b'+' => Problem::Add(inputs),
+                        b'*' => Problem::Mul(inputs),
                         _ => panic!("invalid op"),
                     })
                     .collect();
-
-                return problems;
             }
 
-            for (i, n) in line.split_whitespace().enumerate() {
+            for (i, n) in line
+                .split(|b| *b == b' ')
+                .filter(|slice| !slice.is_empty())
+                .enumerate()
+            {
                 if i >= problem_inputs.len() {
                     problem_inputs.push(Vec::new())
                 }
 
-                let n = n.parse().unwrap();
-                problem_inputs[i].push(n)
+                problem_inputs[i].push(bytes_to_u32(n))
             }
         }
 
@@ -54,33 +69,16 @@ impl Day06 {
     }
 
     fn parse_input_part2(&self) -> Vec<Problem> {
-        let lines = self
-            .input
-            .lines()
-            .filter_map(|l| {
-                if l.is_empty() {
-                    None
-                } else {
-                    Some(l.bytes().collect_vec())
-                }
-            })
-            .collect_vec();
-
         let mut problems = Vec::new();
 
         let mut current_problem = Vec::new();
         let mut digits = Vec::new();
         let mut current_op = None;
-        for col in 0..lines[0].len() {
+        for col in 0..self.lines[0].len() {
             digits.clear();
-            digits.extend((0..lines.len() - 1).filter_map(|row| {
-                let b = lines[row][col];
-                if b.is_ascii_digit() {
-                    let d = b - b'0';
-                    Some(d as u32)
-                } else {
-                    None
-                }
+            digits.extend((0..self.lines.len() - 1).filter_map(|row| {
+                let b = self.lines[row][col];
+                if b.is_ascii_digit() { Some(b) } else { None }
             }));
 
             if digits.is_empty() {
@@ -95,16 +93,9 @@ impl Day06 {
                 continue;
             }
 
-            let pow = (digits.len() - 1) as u32;
-            let current_number = digits
-                .iter()
-                .copied()
-                .enumerate()
-                .map(|(i, d)| 10u32.pow(pow - i as u32) * d)
-                .sum();
-            current_problem.push(current_number);
+            current_problem.push(bytes_to_u32(&digits));
 
-            let last = lines.last().unwrap()[col];
+            let last = self.lines.last().unwrap()[col];
             if matches!(last, b'+' | b'*') {
                 current_op = Some(last);
             }
@@ -123,7 +114,11 @@ impl Day06 {
 
 impl Solution for Day06 {
     fn with_input(input: String) -> Self {
-        Self { input }
+        let lines = input
+            .lines()
+            .filter_map(|l| (!l.is_empty()).then(|| l.bytes().collect_vec()))
+            .collect();
+        Self { lines }
     }
 
     fn part1(&self) -> String {
